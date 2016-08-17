f7c89ef3de6217bde8670a4e3f278e3ab88b8ac6
Joao Esperancinha <jofisaes@hotmail.com>
2016-08-17 10:13:32 +0200

    minify for java script

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,8 @@ module.exports = {
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false}),
-        new ExtractTextPlugin("[name].css")
+    ],
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({ mangle: true, sourcemap: false}),
     ]
 }
