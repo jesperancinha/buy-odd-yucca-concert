e931ba7d8a673069d6919b8b638d3c09ef19134b
Joao Esperancinha <jofisaes@hotmail.com>
2016-08-18 12:21:59 +0200

    timer output implementation

--- src/js/components/ShopButtons.js
+++ src/js/components/ShopButtons.js
@@ -25,4 +25,3 @@ export default class ShopButtons extends React.Component {
     );
   }
 }
-
--- src/js/shop.js
+++ src/js/shop.js
@@ -2,23 +2,48 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import ShopButtons from './components/ShopButtons';
 import Button from 'react-bootstrap/lib/Button';
+import Label from 'react-bootstrap/lib/Label';
 import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
 import Bootstrap from './../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 class Shop extends React.Component{
+    count(e){
+      var counterValue = this.state.counter;
+      counterValue++;
+      this.setState({counter: counterValue})
+    }
+    decount(e){
+      var counterValue = this.state.counter;
+      counterValue--;
+      this.setState({counter: counterValue})
+    }
     constructor(){
         super();
         this.item1 = "Twisted glass";
         this.value1 = 143;
+        this.state = { counter : 1000 }
+        setTimeout(() => {
+            this.setState({ counter: 2 });
+        }, 1000);
+        setInterval(() => {
+            this.decount(this);
+        }, 1000);
     }
+
+
     render() {
         return (
             <div>
+            {this.counter}
                 <h1>Welcome to my webshop</h1>
-                <p>{this.item1}</p><p>{this.value1}</p>
+                <p>{this.item1}</p>
+                <p>{this.value1}</p>
+                <p>{this.state.counter}</p>
+                <Label bsStyle="primary">{this.state.counter}</Label>
                 <ButtonToolbar>
                     <Button bsStyle="primary">Main page</Button>
                     <Button bsStyle="info">About us!</Button>
+                    <Button onClick={this.count.bind(this)} bsStyle="warning">Increase Counter</Button>
                 </ButtonToolbar>
              </div>
         )
