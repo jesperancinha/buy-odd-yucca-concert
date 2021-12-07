e43725ac1ba31038fd44ee7b431e76f8467cdee1
Joao Esperancinha <jofisaes@gmail.com>
2021-12-08 00:29:39 +0100

    🌿 Serving Parking Service via Kong

--- buy-oyc-parking-service/src/main/kotlin/org/jesperancinha/concert/buy/oyc/ParkingServiceLauncher.kt
+++ buy-oyc-parking-service/src/main/kotlin/org/jesperancinha/concert/buy/oyc/ParkingServiceLauncher.kt
@@ -2,11 +2,12 @@ package org.jesperancinha.concert.buy.oyc
 
 import io.micronaut.runtime.Micronaut
 
-class ParkingServiceLauncher
-
-fun main(args: Array<String>) {
-    Micronaut.build()
-        .args(*args)
-        .packages("org.jesperancinha.concert.buy.oyc")
-        .start()
+object ParkingServiceLauncher {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+            .args(*args)
+            .packages("org.jesperancinha.concert.buy.oyc")
+            .start()
+    }
 }
---
+++ kong/Readme.md
@@ -0,0 +1,6 @@
+# Buy Odd Yucca Concert Kong Configuration and Tests
+
+## Test Endpoints
+
+1. [http://localhost:8001/config](http://localhost:8001/config) - Kong Endpoint Configuration
+2. [http://localhost:8001/services/buy-oyc-parking-service/routes](http://localhost:8001/services/buy-oyc-parking-service/routes) - Kong Routes
