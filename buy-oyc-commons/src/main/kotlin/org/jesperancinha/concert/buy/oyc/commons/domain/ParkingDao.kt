7b2e29dad271277152b57e89e6b08e4ff2abf72a
Joao Esperancinha <jofisaes@gmail.com>
2021-12-14 00:40:36 +0100

    🌿 Adds reactive access to PostgreSQL

--- buy-oyc-commons/src/main/kotlin/org/jesperancinha/concert/buy/oyc/commons/domain/ParkingDao.kt
+++ buy-oyc-commons/src/main/kotlin/org/jesperancinha/concert/buy/oyc/commons/domain/ParkingDao.kt
@@ -1,37 +1,25 @@
 package org.jesperancinha.concert.buy.oyc.commons.domain
 
-import io.micronaut.core.annotation.Introspected
-import io.micronaut.data.annotation.Repository
+import io.micronaut.data.annotation.GeneratedValue
+import io.micronaut.data.annotation.Id
+import io.micronaut.data.annotation.MappedEntity
+import io.micronaut.data.model.naming.NamingStrategies
+import io.micronaut.data.model.query.builder.sql.Dialect
+import io.micronaut.data.r2dbc.annotation.R2dbcRepository
 import io.micronaut.data.repository.CrudRepository
-import org.hibernate.Hibernate
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
 
-@Entity
-@Introspected
+@MappedEntity(value = "parking_reservation", namingStrategy = NamingStrategies.UnderScoreSeparatedLowerCase::class)
 data class ParkingReservation(
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-     val id: Long? = null,
-     val parkingNumber: Long
+    @GeneratedValue(value = GeneratedValue.Type.AUTO)
+    val id: Long? = null,
+    val parkingNumber: Long,
 ) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
-        other as ParkingReservation
-
-        return id != null && id == other.id
-    }
-
-    override fun hashCode(): Int = javaClass.hashCode()
-
     @Override
     override fun toString(): String {
         return this::class.simpleName + "(id = $id , parkingNumber = $parkingNumber )"
     }
 }
 
-@Repository
+@R2dbcRepository(dialect = Dialect.POSTGRES)
 interface ParkingReservationRepository : CrudRepository<ParkingReservation, Long>
