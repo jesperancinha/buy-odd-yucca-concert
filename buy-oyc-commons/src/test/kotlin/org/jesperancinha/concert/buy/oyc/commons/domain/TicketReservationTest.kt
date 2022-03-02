b9b884728cc2c75499beab5a43262b367508161b
Joao Esperancinha <jofisaes@gmail.com>
2022-03-02 12:14:37 +0100

    🌿 Coverage in the right place 😊

--- buy-oyc-commons/src/test/kotlin/org/jesperancinha/concert/buy/oyc/commons/domain/TicketReservationTest.kt
+++ buy-oyc-commons/src/test/kotlin/org/jesperancinha/concert/buy/oyc/commons/domain/TicketReservationTest.kt
@@ -3,7 +3,6 @@
 package org.jesperancinha.concert.buy.oyc.commons.domain
 
 import io.kotest.matchers.collections.shouldBeEmpty
-import io.kotest.matchers.collections.shouldNotBeEmpty
 import io.kotest.matchers.comparables.shouldBeEqualComparingTo
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
@@ -15,7 +14,7 @@ import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.configuration.ClassicConfiguration
-import org.jesperancinha.concert.buy.oyc.containers.AbstractBuyOddYuccaConcertContainerTest
+import org.jesperancinha.concert.buy.oyc.containers.AbstractContainerTest
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -32,7 +31,7 @@ class TicketReservationTest @Inject constructor(
     private val ticketRepository: TicketRepository,
     private val parkingReservationRepository: ParkingReservationRepository,
     private val carParkingRepository: CarParkingRepository
-) : AbstractBuyOddYuccaConcertContainerTest() {
+) : AbstractContainerTest() {
 
     private val config = ClassicConfiguration()
 
