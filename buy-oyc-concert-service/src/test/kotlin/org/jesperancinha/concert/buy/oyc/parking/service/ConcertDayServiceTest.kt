5a832321ab489d6b710eb200d0d674738d6fbb12
Joao Esperancinha <jofisaes@gmail.com>
2022-04-21 12:45:56 +0200

    🌿 Removes unnecessary Kotest Extensions

---
+++ buy-oyc-concert-service/src/test/kotlin/org/jesperancinha/concert/buy/oyc/parking/service/ConcertDayServiceTest.kt
@@ -0,0 +1,16 @@
+package org.jesperancinha.concert.buy.oyc.parking.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ConcertDayServiceTest {
+
+    @Test
+    fun createConcertDayReservation() {
+    }
+
+    @Test
+    fun getAll() {
+    }
+}
