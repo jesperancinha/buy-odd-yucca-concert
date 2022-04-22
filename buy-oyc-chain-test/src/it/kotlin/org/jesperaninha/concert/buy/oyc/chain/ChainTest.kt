aa9c4eca1f1ced9afa2977c02d0b61d0742cebee
Joao Esperancinha <jofisaes@gmail.com>
2022-04-22 21:06:38 +0200

    🌿 🇺🇦 Adds foundations of ChainTest containers

---
+++ buy-oyc-chain-test/src/it/kotlin/org/jesperaninha/concert/buy/oyc/chain/ChainTest.kt
@@ -0,0 +1,15 @@
+package org.jesperaninha.concert.buy.oyc.chain
+
+import org.jesperaninha.concert.buy.oyc.containers.AbstractContainersTest
+import org.junit.jupiter.api.Test
+
+/**
+ * Created by jofisaes on 22/04/2022
+ */
+class ChainTest : AbstractContainersTest() {
+
+    @Test
+    fun `should run chain test and create a concert reservation`(){
+
+    }
+}
---
+++ buy-oyc-chain-test/src/it/kotlin/org/jesperaninha/concert/buy/oyc/containers/AbstractContainersTest.kt
@@ -0,0 +1,21 @@
+package org.jesperaninha.concert.buy.oyc.containers
+
+import org.testcontainers.containers.DockerComposeContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import java.io.File
+
+private class DockerCompose(files: List<File>) : DockerComposeContainer<DockerCompose>(files)
+
+/**
+ * Created by jofisaes on 22/04/2022
+ */
+abstract class AbstractContainersTest {
+    companion object {
+        init {
+            DockerCompose(listOf(File("../docker-compose.yml")))
+                .withExposedService("kong", 8000, Wait.defaultWaitStrategy())
+                .withLocalCompose(true)
+                .also { it.start() }
+        }
+    }
+}
