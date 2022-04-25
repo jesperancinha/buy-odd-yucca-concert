f98643f1a761c3091c23441a749f2b264d15958e
Joao Esperancinha <jofisaes@gmail.com>
2022-04-25 13:38:17 +0200

    🌿 🇺🇦 Removes DB waiting from test containers. It waits in docker compose anyways.

--- buy-oyc-commons/src/it/kotlin/org/jesperaninha/concert/buy/oyc/chain/ChainTest.kt
+++ buy-oyc-commons/src/it/kotlin/org/jesperaninha/concert/buy/oyc/chain/ChainTest.kt
@@ -5,6 +5,7 @@ import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import org.jesperancinha.concert.buy.oyc.commons.domain.*
 import org.jesperaninha.concert.buy.oyc.containers.AbstractContainersTest
+import org.jesperaninha.concert.buy.oyc.containers.CustomContextBuilder
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 
@@ -24,22 +25,5 @@ class ChainTest @Inject constructor(
 
     @Test
     fun `should run chain test and create a concert reservation`() {
-    }
-}
-
-class CustomContextBuilder : DefaultApplicationContextBuilder() {
-    init {
-        eagerInitSingletons(true)
-        val serviceHost = AbstractContainersTest.dockerCompose.getServiceHost("db_1", 5432)
-        val props = mapOf(
-            "r2dbc.datasources.default.url" to "r2dbc:postgresql://kong@$serviceHost:5432/yucca?currentSchema=ticket"
-        )
-        logger.info("Database Host configuration is $props")
-
-        properties(props)
-    }
-
-    companion object {
-        private val logger = LoggerFactory.getLogger(CustomContextBuilder::class.java)
     }
 }
--- buy-oyc-commons/src/it/kotlin/org/jesperaninha/concert/buy/oyc/containers/AbstractContainersTest.kt
+++ buy-oyc-commons/src/it/kotlin/org/jesperaninha/concert/buy/oyc/containers/AbstractContainersTest.kt
@@ -1,9 +1,10 @@
 package org.jesperaninha.concert.buy.oyc.containers
 
+import io.micronaut.context.DefaultApplicationContextBuilder
+import org.jesperaninha.concert.buy.oyc.containers.AbstractContainersTest.Companion.dockerCompose
 import org.slf4j.LoggerFactory
 import org.testcontainers.containers.DockerComposeContainer
 import org.testcontainers.containers.wait.strategy.Wait.defaultWaitStrategy
-import org.testcontainers.containers.wait.strategy.Wait.forHealthcheck
 import java.io.File
 
 
@@ -18,11 +19,11 @@ abstract class AbstractContainersTest {
         private val file2 = File("docker-compose-it.yml")
         private val finalFile = if (file1.exists()) file1 else file2
         private val logger = LoggerFactory.getLogger(AbstractContainersTest::class.java)
+
         @JvmStatic
         val dockerCompose: DockerCompose = DockerCompose(listOf(finalFile))
             .withExposedService("redis_1", 6379, defaultWaitStrategy())
             .withExposedService("kong_1", 8001, defaultWaitStrategy())
-            .withExposedService("db_1", 5432, defaultWaitStrategy())
             .withExposedService("buy-oyc-ticket_1", 8084, defaultWaitStrategy())
             .withExposedService("buy-oyc-concert_1", 8085, defaultWaitStrategy())
             .withExposedService("buy-oyc-parking_1", 8086, defaultWaitStrategy())
@@ -38,4 +39,20 @@ abstract class AbstractContainersTest {
             }
     }
 
+}
+
+class CustomContextBuilder : DefaultApplicationContextBuilder() {
+    init {
+        eagerInitSingletons(true)
+        val serviceHost = dockerCompose.getServiceHost("db_1", 5432)
+        val props = mapOf(
+            "r2dbc.datasources.default.url" to "r2dbc:postgresql://kong@$serviceHost:5432/yucca?currentSchema=ticket"
+        )
+        logger.info("Database Host configuration is $props")
+        properties(props)
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(CustomContextBuilder::class.java)
+    }
 }
