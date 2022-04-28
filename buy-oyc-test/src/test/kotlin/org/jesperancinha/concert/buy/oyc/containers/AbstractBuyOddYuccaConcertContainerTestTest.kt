d1d77c8f4c7f11199f0f9b15136f720450988275
Joao Esperancinha <jofisaes@gmail.com>
2022-04-28 23:24:15 +0200

    🌿 🇺🇦 🕊 Coverage for the test library

---
+++ buy-oyc-test/src/test/kotlin/org/jesperancinha/concert/buy/oyc/containers/AbstractBuyOddYuccaConcertContainerTestTest.kt
@@ -0,0 +1,11 @@
+package org.jesperancinha.concert.buy.oyc.containers
+
+import org.junit.Test
+
+internal class AbstractBuyOddYuccaConcertContainerTestTest : AbstractBuyOddYuccaConcertContainerTest() {
+
+    @Test
+    fun `should run and top`() {
+
+    }
+}
