9b11ce888fad1bc316a040e9d922673c8f85da3d
Joao Esperancinha <jofisaes@gmail.com>
2022-05-03 11:05:14 +0200

    🌿🇺🇦🕊 Test data

--- docker-images/docker-psql/multiple/data-yucca.sql
+++ docker-images/docker-psql/multiple/data-yucca.sql
@@ -10,7 +10,16 @@ insert into ticket.car_parking (id, parking_number)
 values ('61BF1E61-A7C2-4D6E-951A-8E97BD4E4FB3', 5);
 
 insert into ticket.concert_day(id, name, description, concert_date)
-values ('5359A368-CA49-4027-BC25-F375E3EA2463',"Jamala", "", now());
+values ('5359A368-CA49-4027-BC25-F375E3EA2463', 'Jamala', '', now());
 insert into ticket.concert_day(id, name, description, concert_date)
-values ('2E4522B1-D9FF-4B2B-9FFA-052CBAD9D5F2',"Kalush Orchestra", "", now());
+values ('2E4522B1-D9FF-4B2B-9FFA-052CBAD9D5F2', 'Kalush Orchestra', '', now());
 
+insert into ticket.drink(id, name, width, height, shape, volume, price)
+values ('2377198D-9E41-4134-8E89-ABD66FE0C59B', 'Varenukha', 10, 10, 10, 10, 10);
+insert into ticket.drink(id, name, width, height, shape, volume, price)
+values ('B2A5E349-76E7-4CD6-8105-308D1BC94953', 'Uzvar', 10, 10, 10, 10, 10);
+
+insert into ticket.meal(id, coupon , box_type, discount, price, processed)
+values ('59B97053-37CF-4FAF-AB50-E77CEF8E8CC8', gen_random_uuid(), 'XS', 10, 10, false);
+insert into ticket.meal(id, coupon , box_type, discount, price, processed)
+values ('4581DECF-7740-44E8-8B2C-B7EC0FEE31C3', gen_random_uuid(), 'XS', 10, 10, false);
