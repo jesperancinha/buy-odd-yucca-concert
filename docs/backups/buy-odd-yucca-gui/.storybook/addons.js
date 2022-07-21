b4885a8c5e0d554783fa1efe5056fa3d8c71f7b4
Joao Esperancinha <jofisaes@gmail.com>
2022-07-21 17:07:24 +0100

    Cleanup

--- docs/backups/buy-odd-yucca-gui/.storybook/addons.js
+++
@@ -1,4 +0,0 @@
-import '@kadira/storybook/addons';
-import 'storybook-addon-specifications/register';
-import '@kadira/storybook-addon-knobs/register'
-import '@kadira/storybook-addon-options/register';
--- docs/backups/buy-odd-yucca-gui/stories/Product/utils.spec.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      expect(-1).to.equal([1, 2, 3].indexOf(4));
-    });
-  });
-});
