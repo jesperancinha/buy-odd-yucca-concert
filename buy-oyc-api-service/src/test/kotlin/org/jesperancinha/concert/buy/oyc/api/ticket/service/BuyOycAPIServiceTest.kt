5cc9a85f48434c9479051266569dd7f262cdfe8a
Joao Esperancinha <jofisaes@gmail.com>
2022-08-09 08:24:20 +0200

    General tests Fix

--- buy-oyc-api-service/src/test/kotlin/org/jesperancinha/concert/buy/oyc/api/ticket/service/BuyOycAPIServiceTest.kt
+++ buy-oyc-api-service/src/test/kotlin/org/jesperancinha/concert/buy/oyc/api/ticket/service/BuyOycAPIServiceTest.kt
@@ -4,13 +4,14 @@ import io.kotest.matchers.booleans.shouldBeTrue
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
+import org.jesperancinha.concert.buy.oyc.containers.AbstractBuyOddYuccaConcertContainerTest
 import org.junit.jupiter.api.Test
 
 @MicronautTest
 class BuyOycAPIServiceTest(
     @Inject
     val application: EmbeddedApplication<*>
-) {
+) : AbstractBuyOddYuccaConcertContainerTest() {
 
     @Test
     fun testItWorks() {
