e3c01ba1596010632243141da9b87b1b32a4b442
Joao Esperancinha <jofisaes@gmail.com>
2022-12-14 19:45:34 +0100

    Cypress completion

--- e2e/cypress/e2e/boyc/ticket.reservation.cy.ts
+++ e2e/cypress/e2e/boyc/ticket.reservation.cy.ts
@@ -1,4 +1,13 @@
 describe('empty spec', () => {
+  const host = Cypress.env('host') ? Cypress.env('host') : 'localhost';
+
   it('passes', () => {
+    cy.request(`http://${host}:8000/api/yucca-ticket`, {
+      headers: {
+        "content-type": "text/html"
+      }
+    }).then(response => {
+      expect(response.body.second).to.be.eq("Welcome to the Ticket service");
+    })
   })
 })
