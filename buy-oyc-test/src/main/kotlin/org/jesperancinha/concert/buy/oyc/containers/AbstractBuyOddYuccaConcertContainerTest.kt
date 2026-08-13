e1052dd9776ceb937dda52924ed4f7c43f44f119
jesperancinha <jofisaes@gmail.com>
2026-08-13 15:43:39 +0200

    Fixes a great deal of tests

--- buy-oyc-test/src/main/kotlin/org/jesperancinha/concert/buy/oyc/containers/AbstractBuyOddYuccaConcertContainerTest.kt
+++ buy-oyc-test/src/main/kotlin/org/jesperancinha/concert/buy/oyc/containers/AbstractBuyOddYuccaConcertContainerTest.kt
@@ -1,13 +1,9 @@
 package org.jesperancinha.concert.buy.oyc.containers
 
-import com.github.dockerjava.api.model.ExposedPort
-import com.github.dockerjava.api.model.HostConfig
-import com.github.dockerjava.api.model.PortBinding
-import com.github.dockerjava.api.model.Ports.Binding.bindPort
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.configuration.ClassicConfiguration
 import org.testcontainers.containers.GenericContainer
-import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.postgresql.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName.parse
 
@@ -18,39 +14,17 @@ private const val REDIS_PORT = 6379
 
 abstract class AbstractBuyOddYuccaConcertContainerTest {
     companion object {
-        @Container
-        @JvmField
         val postgreSQLContainer = TestPostgresSQLContainer("postgres:16-alpine")
             .withUsername("kong")
             .withPassword("kong")
             .withDatabaseName("yucca")
             .withExposedPorts(POSTGRESQL_PORT)
-            .withCreateContainerCmdModifier { cmd ->
-                cmd.withHostConfig(
-                    HostConfig().withPortBindings(
-                        PortBinding(
-                            bindPort(POSTGRESQL_PORT),
-                            ExposedPort(POSTGRESQL_PORT)
-                        )
-                    )
-                )
-            }.also { it.self().start() }
+            .also { it.start() }
 
 
-        @Container
-        @JvmField
         val redis: GenericContainer<*> = GenericContainer(parse("redis:5.0.3-alpine"))
             .withExposedPorts(REDIS_PORT)
-            .withCreateContainerCmdModifier { cmd ->
-                cmd.withHostConfig(
-                    HostConfig().withPortBindings(
-                        PortBinding(
-                            bindPort(REDIS_PORT),
-                            ExposedPort(REDIS_PORT)
-                        )
-                    )
-                )
-            }.also { it.self().start() }
+            .also { it.start() }
 
         val config = ClassicConfiguration()
 
@@ -63,6 +37,16 @@ abstract class AbstractBuyOddYuccaConcertContainerTest {
             )
             config.schemas = arrayOf("ticket")
             Flyway(config).migrate()
+            postgreSQLContainer.waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*\\s", 1))
+            redis.waitingFor(Wait.forLogMessage(".*Ready to accept connections.*\\s", 1))
+            System.setProperty(
+                "r2dbc.datasources.default.url",
+                "r2dbc:postgresql://kong@${postgreSQLContainer.host}:${postgreSQLContainer.getMappedPort(POSTGRESQL_PORT)}/yucca?currentSchema=ticket"
+            )
+            System.setProperty(
+                "redis.uri",
+                "redis://${redis.host}:${redis.getMappedPort(REDIS_PORT)}"
+            )
         }
     }
 }
